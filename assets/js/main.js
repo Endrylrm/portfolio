1499aaf5565956b0370a2be4a4fa681341fa4fb6
Endryl Richard Monteiro <endrylrm@hotmail.com>
2024-12-12 23:11:37 -0300

    feat: usar o localStorage para lembrar o tema escolhido.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,31 @@ const rootHtml = document.documentElement;
 
 themeSwitcher.addEventListener("click", changeTheme);
 
+function switchSunMoon(theme) {
+  themeSwitcher.classList.remove("bi-sun");
+  themeSwitcher.classList.remove("bi-moon-stars");
+  theme === "dark"
+    ? themeSwitcher.classList.add("bi-sun")
+    : themeSwitcher.classList.add("bi-moon-stars");
+}
+
 function changeTheme() {
   let currentTheme = rootHtml.dataset.theme;
 
-  themeSwitcher.classList.toggle("bi-sun");
-  themeSwitcher.classList.toggle("bi-moon-stars");
-
   if (currentTheme === "dark") {
     rootHtml.dataset.theme = "light";
+    localStorage.setItem("theme", "light");
   } else {
     rootHtml.dataset.theme = "dark";
+    localStorage.setItem("theme", "dark");
   }
+
+  switchSunMoon(rootHtml.dataset.theme);
+}
+
+if (localStorage.getItem("theme") != null) {
+  rootHtml.dataset.theme = localStorage.getItem("theme");
+  switchSunMoon(rootHtml.dataset.theme);
 }
 
 const desktopMenuLinks = document.querySelectorAll(
